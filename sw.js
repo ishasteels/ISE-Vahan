8eb667035dbe2a5b581514e85121052d0b994b3b
Isha Steels Enterprises <sysdev@ishasteels.com>
2026-07-10 15:23:54 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'ise-voms-v3';
+var CACHE = 'ise-voms-v5';
 var SHELL = ['./', './index.html', './app.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(e) {
